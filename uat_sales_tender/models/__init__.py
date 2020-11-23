cf59216b52f6df9f6a0b40577bbd3c0d34e99dd4
Mohammed Alwasefy <mohammed.alwasefy@gmail.com>
2020-11-23 14:57:32 +0200

    Add files via upload

---
+++ uat_sales_tender/models/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+from . import sales_tender
+from . import tender_documents
+from . import product_tender_documents
+from . import supplier_tender
+from . import supplier_documents
+from . import calverter
